0bee42801847bd02f37815027be545f9a7b48743
Gurkirat17007 <41137077+Gurkirat17007@users.noreply.github.com>
2018-09-21 17:32:35 -0400

    Initial commit

---
+++ comp2084/comp2084/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="comp2084.MvcApplication" Language="C#" %>
---
+++ comp2084/comp2084/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(comp2084.Startup))]
+namespace comp2084
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
